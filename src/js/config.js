5bebd33186fbb7ff2e33c828d626ba7697778435
amiola <agumiola@gmail.com>
2023-02-16 11:00:57 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '28b5664a-788e-42f6-9c13-e93e520d4074';
+export const MODAL_CLOSE_SEC = 2.5;
